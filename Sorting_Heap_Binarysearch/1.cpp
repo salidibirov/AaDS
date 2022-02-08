b46487e4223d68673f749378227ea15116b4b1bc
salidibirov <said.alidibirov.13@gmail.com>
2022-02-08 15:02:56 +0300

    Update 1.cpp

---
+++ Sorting_Heap_Binarysearch/1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <limits>
+#include <tuple>
+#include <iomanip>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <memory>
+#include <set>
+#include <map>
+#include <exception>
+#include <sstream>
+#include <utility>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    return 0;
+}
+AaDS
+Solving problems on Algorithms and Data Structures(AaDS)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
