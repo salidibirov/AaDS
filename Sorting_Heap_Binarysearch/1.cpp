eed207c6d6e42d0d6a1c12ddf02a53940b4269b7
salidibirov <said.alidibirov.13@gmail.com>
2022-02-09 14:00:11 +0300

    First Task

--- Sorting_Heap_Binarysearch/1.cpp
+++ Sorting_Heap_Binarysearch/1.cpp
@@ -29,10 +29,26 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
+    // H/W English 95-96 Ex 5, 6, 7, 8, 2 Garagylya
+    // 100-102 Ex 10-17
+
+    vector<int> v = {8, 7, 6, 5, 4, 3, 2, 1};
+    //Сортировка вставками
+    for (int i = 1; i < 5; ++i) {
+        int j = i;
+        while (j > 0 && v[j] < v[j - 1]) {
+            swap(v[j], v[j - 1]);
+            j--;
+        }
+    }
+
+    for (const auto& i : v) {
+        cout << i << ' ';
+    }
+
     return 0;
 }
-AaDS
-Solving problems on Algorithms and Data Structures(AaDS)
+
 
 
 
